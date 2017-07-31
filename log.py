82d08bd414cb719da61997bc950939734aa22019
Felipe Samuel <samukajobs@gmail.com>
2017-07-31 15:47:02 -0300

    Update log.py

--- log.py
+++ log.py
@@ -1,29 +1,25 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
+from seumodulo import app
 from datetime import datetime
-import sys, traceback
+import sys
+import traceback
 
+class Log(object):
+    
+    def __init__(self):
+        self.__caminho = app.config['LOG_URI']
+        self.__exibe = app.config['DEBUG']
 
-class Log:
-    diretorio = os.getcwd()+ "\\log.log"
-
-    def __init__(self, caminho = diretorio):
-        self.caminho = caminho
-
-    def log(self):
+    def logging(self):
         try:
-             exc_type, exc_value, exc_traceback =  sys.exc_info()
-             erro = traceback.format_exception(exc_type, exc_value,exc_traceback)
+            exc_type, exc_value, exc_traceback =  sys.exc_info()
+            erro = traceback.format_exception(exc_type, exc_value,exc_traceback)
 
-             with open(self.caminho, "a") as arquivo:
-                arquivo.write("\n"+str(datetime.now())+" => "+str(erro[1]+"\n\t"+str(erro[2])))
-        except:
-            print("Falha ao salvar o arquivo de log")
+            with open(self.__caminho, "a") as arquivo:
+               conteudo = str(datetime.now())+" => "+str(erro[1]+"\n\t"+str(erro[2]))+"\n"
+               conteudo += "************************************************************************************************************************************************************\n"
+               arquivo.write(conteudo)
 
-    def ler(self):
-        try:
-            return open(self.caminho, 'r')
-        except:
-            print("Falha ao ler o arquivo de log.")
-        return []
+            if self.__exibe:
+                print(erro)
+        except Exception as e:
+            print(e)
